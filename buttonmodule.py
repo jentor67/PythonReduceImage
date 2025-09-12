f8ab8304c301fc9cf4454d537509b3f8909a8a20
jentor67 <john.jentor@gmail.com>
2025-09-12 00:25:52 -0500

    more changes

--- buttonmodule.py
+++ buttonmodule.py
@@ -3,12 +3,13 @@ import tkinter as tk
 
 
 class MyButton():
-    def __init__(self, master,title,commandname,r,c,y):
+    def __init__(self, master, title, commandname, r, c, y):
         self.master = master
         self.button_result = None
 
         self.button = tk.Button(master, text=title, command=commandname)
-        self.button.grid(row=r,column=c,pady=y)
+
+        self.button.grid(row=r, column=c, pady=y)
 
     def button_action(self):
         self.button_result = "Data from button click!"
--- imageReducer.py
+++ imageReducer.py
@@ -41,6 +41,7 @@ def pick_converted_image_folder():
 def process_images():
     print("Hello")
     print(output_directory)
+    print(input_directory)
 
 # Create main window
 root = tk.Tk()
@@ -48,27 +49,36 @@ root.title("Folder Picker")
 root.geometry("800x400")
 
 # Button to open folder dialog
-btn_image = buttonmodule.MyButton(root,"Image Folder", "pick_image_folder",0,0,2)
+btn_image = buttonmodule.MyButton(root,"Image Folder", 
+     pick_image_folder,0,0,2)
 
 # Label to display selected folder
 input_label = tk.Label(root, text="No folder selected", 
-                       wraplength=350, justify="center")
+     wraplength=350, justify="center")
+
+
 input_label.grid(row=1, column=0, pady=2)
 
 # Button to open folder dialog
 btn_converted_image = buttonmodule.MyButton(root,"Output Image Folder", 
-                                            "pick_converted_image_folder",2,0,2)
+    pick_converted_image_folder,2,0,2)
 
 # Label to display selected folder
-output_label = tk.Label(root, text="No folder selected", wraplength=350, justify="center")
+output_label = tk.Label(root, text="No folder selected", 
+    wraplength=350, justify="center")
+
 output_label.grid(row=3, column=0, pady=2)
 
 # Button to open folder dialog
-btn_process = tk.Button(root, text="Process Images", command=process_images)
+btn_process = tk.Button(root, text="Process Images", 
+    command=process_images)
+
 btn_process.grid(row=4, column=0, pady=2)
 
-file_listbox = tk.Listbox(root,width=60, height=15)
-file_listbox.grid(row=0, column=2, columnspan=2, rowspan=4, padx= 2,  pady=2)
+file_listbox = tk.Listbox(root, width=60, height=15)
+
+file_listbox.grid(row=0, column=2, columnspan=2, rowspan=4, 
+    padx= 2,  pady=2)
 
 #bbtn_test = buttonmodule.MyButton(root,5,0,2)
 
